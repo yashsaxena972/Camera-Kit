2869ee3a13d03fa47782a96be0e518b5f68eeaa1
Shrirama-Upadhya-A <shriramaupadhya1303@gmail.com>
2021-01-08 14:19:07 +0530

    Edited MainActivity.java, added button features - 1.0

--- app/src/main/java/com/example/hvcamera/MainActivity.java
+++ app/src/main/java/com/example/hvcamera/MainActivity.java
@@ -2,14 +2,18 @@ package com.example.hvcamera;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
+import com.camerakit.CameraKit;
 import com.camerakit.CameraKitView;
 
 public class MainActivity extends AppCompatActivity {
 
     private CameraKitView cameraKitView;
     private ImageButton flipCameraButton, captureButton, toggleFlashButton;
+    private boolean flashStatus = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,36 @@ public class MainActivity extends AppCompatActivity {
         flipCameraButton = findViewById(R.id.flip_camera_button);
         captureButton = findViewById(R.id.capture_button);
         toggleFlashButton = findViewById(R.id.toggle_flash_button);
+    }
+    public void onCaptureClick(View view) {
+        cameraKitView.captureImage(new CameraKitView.ImageCallback() {
+            @Override
+            public void onImage(CameraKitView cameraKitView, final byte[] capturedImage) {
+                Toast.makeText(MainActivity.this, "Image Captured", Toast.LENGTH_SHORT).show();
+                /*File savedPhoto = new File(Environment.getExternalStorageDirectory(), "photo.jpg");
+                try {
+                    FileOutputStream outputStream = new FileOutputStream(savedPhoto.getPath());
+                    outputStream.write(capturedImage);
+                    outputStream.close();
+                } catch (java.io.IOException e) {
+                    e.printStackTrace();
+                }*/
+            }
+        });
+    }
+
+    public void onFlipCamera(View view) {
+        cameraKitView.toggleFacing();
+    }
+
+    public void onFlashButtonClick(View view){
+        /*if(flashStatus){
+            cameraKitView.setFlash(CameraKit.FLASH_ON);
+        }
+        else{
+            cameraKitView.setFlash(CameraKit.FLASH_OFF);
+        }
+        flashStatus = !flashStatus;*/
     }
 
     @Override
@@ -51,4 +85,7 @@ public class MainActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         cameraKitView.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
+
+
+
 }
