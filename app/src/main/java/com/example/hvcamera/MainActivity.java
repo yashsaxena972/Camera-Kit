e551a8dd4459635974feb744f0dae4bc40b530eb
Yash Saxena <yash972saxena@gmail.com>
2021-01-08 13:43:20 +0530

    UI added - 1.1

--- app/src/main/java/com/example/hvcamera/MainActivity.java
+++ app/src/main/java/com/example/hvcamera/MainActivity.java
@@ -2,17 +2,24 @@ package com.example.hvcamera;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+
 import com.camerakit.CameraKitView;
 
 public class MainActivity extends AppCompatActivity {
 
     private CameraKitView cameraKitView;
+    private ImageButton flipCameraButton, captureButton, toggleFlashButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         cameraKitView = findViewById(R.id.camera);
+
+        flipCameraButton = findViewById(R.id.flip_camera_button);
+        captureButton = findViewById(R.id.capture_button);
+        toggleFlashButton = findViewById(R.id.toggle_flash_button);
     }
 
     @Override
